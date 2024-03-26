0d24172930de7d19062f4f6c8493726768e2b9e3
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-03-26 15:39:24 +0200

    created initial solution

--- ASSIGNMENT_1/SA.java
+++ ASSIGNMENT_1/SA.java
@@ -1,5 +1,44 @@
 package ASSIGNMENT_1;
 
+import java.util.Random;
+
 public class SA {
+    int[][] disntanceCost;
+
+    //A method to generate an initial solution.
+    public int[] initialSolution()
+    {
+        //create an populate arr
+        int[] currSolution= new int[5];
+        for(int i=0; i<5; i++){
+            currSolution[i]=i;
+        }
+
+        //randomise arr
+        Random r=new Random();
+        
+        for (int i=4; i>0; i--)
+        {
+            int j=r.nextInt(i+1);
+            int tmp=currSolution[i];
+            currSolution[i]=currSolution[j];
+            currSolution[j]=tmp;
+        }
+        return currSolution;
+    }
+
+    //A generation function to find neighbours 
+    public int[] findNeighbours(int index)
+    {
+        return null;
+    }
+
+    //A cost function.
+    public int cost(int[] arr)
+    {
+        return -1;
+    }
+
+    //An evaluation criterion.
     
 }
