16fffa482922407f4d1d94c85577186829dc2557
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-03-26 13:11:29 +0200

    added campuses class

---
+++ ASSIGNMENT_1/campuses.java
@@ -0,0 +1,16 @@
+package ASSIGNMENT_1;
+//ayandajuqu
+//u21589021
+public class campuses{
+    private String campusName;
+
+    public String getName()
+    {
+        return campusName;
+    }
+
+    public void setName(String name)
+    {
+        campusName=name;
+    }
+}
