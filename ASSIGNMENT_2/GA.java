00e78d7bd996a08691e8243c9c9f44292b75bbea
ayandajuqu <ms.ayandajuqu@gmail.com>
2024-04-22 22:44:13 +0200

    formatted

--- ASSIGNMENT_2/GA.java
+++ ASSIGNMENT_2/GA.java
@@ -1,4 +1,17 @@
 public class GA {
 
+    /*1: Create initial population
+2: Calculate fitness of all individuals
+3: while termination condition not met do
+4:   Select fitter individuals for reproduction
+5:   Recombine individuals
+6:   Mutate individuals
+7:   Evaluate fitness of all individuals
+8:   Generate a new population
+9:   
+10: end while
+11: return best individual */
+
+//create initial popularion
     
 }
